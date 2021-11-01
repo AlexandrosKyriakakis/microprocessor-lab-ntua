923bebe80b8d82073a0b4c465d7d259262090e51
Alexandros Kyriakakis <hmmyalex@gmail.com>
2021-11-01 22:14:15 +0200

    First/Last Commit

--- Lab_5/RVfpga/verilatorSIM/jtagServer.d
+++
@@ -1,3 +0,0 @@
-jtagServer.o: ../src/OtherSources/jtag_vpi_0-r5/jtagServer.cpp \
- ../src/OtherSources/jtag_vpi_0-r5/jtagServer.h \
- ../src/OtherSources/jtag_vpi_0-r5/jtag_common.h
--- Lab_5/RVfpga/verilatorSIM/tb.d
+++
@@ -1,8 +0,0 @@
-tb.o: ../src/OtherSources/tb.cpp \
- ../src/OtherSources/jtag_vpi_0-r5/jtagServer.h \
- ../src/OtherSources/jtag_vpi_0-r5/jtag_common.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated.h Vrvfpgasim.h \
- /usr/local/share/verilator/include/verilated_heavy.h Vrvfpgasim__Dpi.h \
- /usr/local/share/verilator/include/vltstd/svdpi.h
